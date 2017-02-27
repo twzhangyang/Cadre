4dc65c9e7f8a9e77e912c9f5cb4d417a47ed2ffd
Yang <yrzhang@thoughtworks.com>
2017-02-27 13:25:54 +0800

    first commit

---
+++ Src/CadreManagement/CadreManagement.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CadreManagement.Web.MvcApplication" Language="C#" %>
---
+++ Src/CadreManagement/CadreManagement.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CadreManagement.Web.Startup))]
+namespace CadreManagement.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
